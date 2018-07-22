718e54c1598ef8d4bf6ae601286ff4867f7b5801
tomoko.nomura <snowflake.58142@nifty.com>
2018-07-22 03:59:09 +0000

    bootstrap design

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -5,14 +5,27 @@
     <h1>メッセージ一覧</h1>
     
     @if (count($messages) > 0)
-        <ul>
-            @foreach ($messages as $message)
-                <li>{!! link_to_route('messages.show', $message->id, ['id' => $message->id]) !!} : {{ $message->title }} > {{ $message->content }}</li>
-            @endforeach
-        </ul>
+        <table class="table table-striped">
+            <tread>
+                <tr>
+                    <th>id</th>
+                    <th>タイトル</th>
+                    <th>メッセージ</th>
+                </tr>
+            </tread>
+            <tbody>
+                @foreach ($messages as $message)
+                    <tr>
+                        <td>{!! link_to_route('messages.show', $message->id, ['id' => $message->id]) !!}</td>
+                        <td>{{ $message->title }}</td>
+                        <td width="200">{{ $message->content }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     @endif
     
-    {!! link_to_route('messages.create', '新規メッセージの投稿') . '<br><br>' !!}
+    {!! link_to_route('messages.create', '新規メッセージの投稿', null, ['class' => 'btn btn-primary']) . '<br><br>' !!}
 
     {{ '<p style="color: red;">htmlentities 関数に通した場合</p>' }}
     {!! '<p style="color: red;">htmlentities 関数に通さなかった場合</p>' !!}
